532e7599be422fc4e78b06a87ea9264e02e34e6d
Diego Ferro <daferrom@gmail.com>
2024-12-11 23:35:35 -0500

    feat: Added Inital Results efficientNEtB0 with transfer learning

---
+++ src/efficientnetb0/training/__init__.py
@@ -0,0 +1,1 @@
+from .train_efficientNetB0_sup_layers import model , history
--- src/nombre_paquete/config/config.py
+++
@@ -1,3 +0,0 @@
-DATASET_PATH_RAW_DATA = "../../../data/raw_data/UCMerced_LandUse/Images"
-
-PREPROCESSED_DATA_PATH = '../../data/images_converted'
