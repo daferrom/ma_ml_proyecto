86a24272464a05948fb4a3aa655251a4ea3b9bb3
Diego Ferro <daferrom@gmail.com>
2024-12-12 23:04:46 -0500

    added hpe exploration evaluation and model_report

---
+++ scripts/eda/config.py
@@ -0,0 +1,19 @@
+DATASET_PATH_RAW_DATA = "../../../data/raw_data/UCMerced_LandUse/Images"
+
+PREPROCESSED_DATA_PATH = '../../data/images_converted'
+
+SPLIT_DATASET = "../../data/splited_dataset"
+
+
+
+BATCH_SIZE = 32
+
+IMG_SIZE = (256, 256)
+
+IMAGE_CHANNELS = (3,)
+
+INPUT_SHAPE = (256, 256) + IMAGE_CHANNELS
+
+ROOT_SEED = 42
+
+NUM_CLASSES = 21
--- src/efficientnetb0/models/__init__.py
+++ src/efficientnetb0/models/__init__.py
@@ -1,1 +1,2 @@
 from .efficientnetB0_initial_arch import train_efficientnetB0_with_transfer_learning
+from ..training.efficientnetB0_hpe_arch import build_model_efficientNetB0_hpe
